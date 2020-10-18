2b34bbdc8fcd4f4debdf7b3e057a52b7fa7a5c70
Qzki <qzki@qq.com>
2020-10-18 23:28:39 +0800

    style: fix eslint issues

--- src/app/__tests__/app.test.js
+++ src/app/__tests__/app.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
+import { render } from '@testing-library/react';
 import App from '../app';
-
-import {render} from '@testing-library/react';
 
 describe('App', () => {
   test('should render App component', () => {
     const { getByTestId } = render(<App />);
-    expect(getByTestId('app').textContent).toBe('Hello World');
-  })
+    expect(getByTestId('app').textContent)
+      .toBe('Hello World');
+  });
 });
